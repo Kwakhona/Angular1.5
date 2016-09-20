2f537f77e4c33d5f42c621632107df86e10cabed
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-20 14:18:51 +0200

    added services

--- src/modules/core/services/services.module.js
+++ src/modules/core/services/services.module.js
@@ -1,4 +1,8 @@
 import AuthenticationService from './authentication.service';
+import MobxServiceExample from './mobx-example.service';
+import RxServiceExample from './ex-example.service';
 
 export default angular.module('core.services', [])
-    .service('AuthenticationService', AuthenticationService);
+    .service('AuthenticationService', AuthenticationService)
+    .service('MobxServiceExample', MobxServiceExample)
+    .service('RxServiceExample', RxServiceExample);
