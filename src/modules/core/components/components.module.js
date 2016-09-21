804c784a5472889d5e4a905ea35acb94c50b300f
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-21 09:41:27 +0200

    added codeclimate yml file

--- src/modules/core/components/components.module.js
+++ src/modules/core/components/components.module.js
@@ -2,9 +2,11 @@ import AppComponent from './app/app.component';
 import LoginComponent from './login/login.component';
 import LogoutComponent from './logout/logout.component';
 import ListComponent from './list/list.component';
+import ContainerFluidComponent from './container-fluid/container-fluid.component';
 
 export default angular.module('core.component', [])
     .component('app', AppComponent)
     .component('login', LoginComponent)
     .component('logout', LogoutComponent)
-    .component('list', ListComponent);
+    .component('list', ListComponent)
+    .component('containerFluid', ContainerFluidComponent);
