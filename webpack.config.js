24785f0ec73d9444f60ac32c505fac0d750b0c8d
Kwakhona <kwakhona@tangentsolutions.co.za>
2016-09-26 10:02:36 +0200

    updated webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: __direname + '/dist',
+        path: __dirname + '/dist',
         filename: 'index.js'
     },
     devtool: 'source-map',
